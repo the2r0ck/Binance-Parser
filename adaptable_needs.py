04030092dfd37260961e81d2bc40cfd7603c5dfc
the2r0ck <mpetrynko@gmail.com>
2024-07-02 16:01:44 +0300

    run crontab endpoint

--- adaptable_needs.py
+++ adaptable_needs.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from fastapi import FastAPI
 
 
@@ -7,4 +9,15 @@ app = FastAPI()
 def hello_world():
     return {
         "message": "Hello World"
+    }
+    
+
+@app.get("/run_cron")
+def hello_world():
+    result = subprocess.run("cron", shell=True, capture_output=True, text=True)
+    return {
+        "message": "cron runned",
+        "stdout": result.stdout,
+        "stderr": result.stderr,
+        "returncode": result.returncode
     }
